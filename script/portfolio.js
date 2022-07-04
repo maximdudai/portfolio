1ea0ef8eb47d77d8c18763abb9b2b421cebcc96d
Flint1101 <maxim.dudai11@gmail.com>
2022-07-04 21:56:14 +0100

    page content with small presentation

---
+++ script/portfolio.js
@@ -0,0 +1,12 @@
+(function() {
+    const menuButton = document.querySelector("[data-menu='button']");
+    const menuList = document.querySelector("[data-menu='list']");
+
+    menuButton.addEventListener('click', openMenu);
+
+    function openMenu() 
+    {
+        menuList.classList.toggle('active')
+        menuButton.classList.toggle('active')
+    }  
+})();
