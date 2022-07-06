594f06042ca0eb8743f8e442acd9472903f78163
maximdudai <maxim.dudai01@gmail.com>
2022-07-06 20:14:48 +0100

    page reload

--- script/portfolio.js
+++ script/portfolio.js
@@ -10,11 +10,9 @@
         menuButton.classList.toggle('active')
     }  
 })();
-
 const arrowUp = document.querySelector('.up-arrow');
 window.onscroll = () => arrowUp.style.visibility = window.scrollY >= 125 ? 'visible' : 'hidden';
 arrowUp.addEventListener("click", () => window.scroll(0, 0));
-
 const checkDefault = (e) => {
     if(window.scrollY === 0.0) {
         e.preventDefault();
